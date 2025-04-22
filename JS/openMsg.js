41b811a3bc3684e3ad7e7c5727aed9dc2a99de93
Tim Danielsen <=>
2025-04-22 16:07:29 -0700

    Added expired session logic

--- JS/openMsg.js
+++ JS/openMsg.js
@@ -1,8 +1,15 @@
-function openMsg(alertmsg,closeOrConfirm) {	
-	   var msgbody = document.getElementById('msgBody');
-	   var button = document.getElementById('msgButton');
-	   msgbody.innerHTML = alertmsg;
+function openMsg(alertmsg,closeOrConfirm,buttonObject) {
+	var msgbody = document.getElementById('msgBody');
+	var button = document.getElementById('msgButton');
+	msgbody.innerHTML = alertmsg;
+	if (typeof buttonObject === 'undefined') {
 	   if (closeOrConfirm == "close") {button.innerHTML = "Close"}
-	   else if (closeOrConfirm == "confirm") {button.innerHTML = "Confirm"}		
-	   document.getElementById('openMsg').click();	
+	   else if (closeOrConfirm == "confirm") {button.innerHTML = "Confirm"}
+	}
+	else {
+	   button.innerHTML = buttonObject.text;
+	   $('#msgButton').attr("onclick",buttonObject.clickEvent);
+	}
+
+	document.getElementById('openMsg').click();
 }
